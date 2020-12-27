e6a02412a07b2d0cd212bdcc01283afadcd99374
Paulo <pauloestrella1994@gmail.com>
2020-12-27 19:53:24 -0300

    Add regex to clean data and udd PyPDF

---
+++ regular_expressions/project/phone_and_email.py
@@ -0,0 +1,35 @@
+import re, PyPDF2
+
+phoneRegex = re.compile(r'''
+                            # some phones examples that we need to find
+                            # 415-555-0000, 555-0000, (415) 555-0000, 555-0000 ext 12345, ext. 12345, x12345
+                            ((\d\d\d) | (\(\d\d\d\)))?    #area code
+                            (\s|-)    #first separator
+                            \d\d\d    #first 3 digits
+                            -    #separator
+                            \d\d\d\d    #last 4 digits
+                            (((ext(\.)?\s)|x) #extension word-part (optional)
+                                (\d{2,5}))?   #extension number-part (optional)
+
+                        ''', re.VERBOSE)
+
+emailRegex = re.compile(r'''
+                        # some.+_thing@(\d{2, 5})?.com
+                        [a-z0-9_.+]+   #name part
+                        @    #@ symbol
+                        [a-z0-9_.+]+   #domain name part
+
+                        ''', re.VERBOSE | re.I)
+
+pdfFile = open('regular_expressions/project/examplePhoneEmailDirectory.pdf', 'rb')
+reader = PyPDF2.PdfFileReader(pdfFile)
+pages = reader.numPages
+
+data = ''
+for pageNum in range(pages):
+    page = reader.getPage(pageNum)
+    data += page.extractText()
+
+subs = re.compile(r'[\n]')
+clean_data = subs.sub("", data)
+print(clean_data)
