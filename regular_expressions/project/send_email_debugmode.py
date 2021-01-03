734cb0e8f0da417599733c89b95340b100687294
Paulo <pauloestrella1994@gmail.com>
2021-01-03 16:38:36 -0300

    Refatoring method to use send.message()

--- regular_expressions/project/send_email_debugmode.py
+++ regular_expressions/project/send_email_debugmode.py
@@ -1,13 +1,15 @@
 import smtplib
+from email.message import EmailMessage
 from prettyconf import config
 
 EMAIL_ADDRESS = config('EMAIL_USER')
 EMAIL_PASSWORD = config('EMAIL_PASSWORD')
 
+msg = EmailMessage()
+msg['Subject'] = 'Just a test'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = EMAIL_ADDRESS
+msg.set_content('This is the body of my test, sending email to myself!')
+
 with smtplib.SMTP('localhost', 1025) as smtp:
-    subject = 'Just a test'
-    body = 'This is the body of my test'
-
-    msg = f'Subject: {subject}\n\n {body}'
-
-    smtp.sendmail(EMAIL_ADDRESS, 'example@gmail.com', msg)
+    smtp.send_message(msg)
